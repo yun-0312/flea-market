a4a0ffc8732104766cb2db12d282f9d9e79a5380
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-10-30 14:37:32 +0900

    likesテーブル→favoriteテーブル

--- src/database/migrations/2025_10_29_044558_create_favorites_table.php
+++ src/database/migrations/2025_10_29_044558_create_favorites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLikesTable extends Migration
+class CreateFavoritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLikesTable extends Migration
      */
     public function up()
     {
-        Schema::create('likes', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->unique();
             $table->foreignId('item_id')->constrained()->onDelete('cascade')->unique();
@@ -28,6 +28,6 @@ class CreateLikesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('likes');
+        Schema::dropIfExists('favorites');
     }
 }
