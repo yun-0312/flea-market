bd16b035c2862698ea5438b67e6e33511b2d86c1
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-10-31 11:56:30 +0900

    blade、cssファイル作成

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
+
+@section('header')
+        @include('layouts.header')
+@endsection
+
+@section('content')
+    <div class="content-wrapper">
+        <h2 class="register-form__hedding">会員登録</h2>
+    </div>
+    
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FashionablyLate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+<body>
+    @yield('header')
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
