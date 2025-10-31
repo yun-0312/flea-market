12f87d6569f38a0b92256d0b556d0e4072bde805
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-10-31 12:00:42 +0900

    css修正

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
 @endsection
 
 @section('header')
