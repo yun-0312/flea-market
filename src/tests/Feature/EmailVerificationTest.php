5a73329bf1365d2c87275bafd8d14edd427259ab
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-11-19 21:03:53 +0900

    EmailVerificationTest修正

--- src/tests/Feature/EmailVerificationTest.php
+++ src/tests/Feature/EmailVerificationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -11,7 +12,6 @@ use Tests\TestCase;
 class EmailVerificationTest extends TestCase
 {
     use RefreshDatabase;
-    
     /** @test */
     //会員登録後、認証メールが送信される
     public function user_receives_email_verification_notification_after_register()
@@ -35,7 +35,8 @@ class EmailVerificationTest extends TestCase
         );
     }
 
-        /** @test */
+    /** @test */
+    //メール認証誘導画面で「認証はこちらから」ボタンを押すとメール認証サイトに遷移する
     public function email_verify_page_is_displayed()
     {
         $user = User::factory()->unverified()->create();
@@ -49,6 +50,7 @@ class EmailVerificationTest extends TestCase
     }
 
     /** @test */
+    //メール認証画面で「認証はこちらから」ボタンを押すとメール認証サイトに遷移する
     public function user_can_request_verification_link()
     {
         $user = User::factory()->unverified()->create();
@@ -60,25 +62,20 @@ class EmailVerificationTest extends TestCase
         $response->assertRedirect();
     }
 
-        /** @test */
-    public function user_receives_email_verification_notification_after_register()
+    /** @test */
+    //メール認証サイトのメール認証を完了すると、プロフィール設定画面に遷移する
+    public function user_is_redirected_to_profile_after_verification()
     {
-        Notification::fake();
+        $user = User::factory()->unverified()->create();
 
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
-
-        $response->assertRedirect('/email/verify');
-
-        $user = User::where('email', 'test@example.com')->first();
-
-        Notification::assertSentTo(
-            $user,
-            VerifyEmail::class
+        $verificationUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            ['id' => $user->id, 'hash' => sha1($user->email)]
         );
+        $response = $this->actingAs($user)->get($verificationUrl);
+
+        $response->assertRedirect('/mypage/profile');
+        $this->assertTrue($user->fresh()->hasVerifiedEmail());
     }
 }
