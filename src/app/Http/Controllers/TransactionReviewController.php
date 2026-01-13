fde636599f3dc486eafe56609692a06f6f306b32
yun-0312 <yumiko_03122000@yahoo.co.jp>
2026-01-14 08:26:28 +0900

    transactionモデル修正

--- src/app/Http/Controllers/TransactionReviewController.php
+++ src/app/Http/Controllers/TransactionReviewController.php
@@ -23,15 +23,15 @@ class TransactionReviewController extends Controller
             'rating' => $request->rating,
         ]);
         if ($transaction->isBuyer($user)) {
-            Mail::to($transaction->seller()->email)
+            Mail::to($transaction->seller->email)
                 ->send(new SellerReviewRequestMail($transaction));
         }
         // 両者評価完了チェック
         $reviewerIds = $transaction->reviews()->pluck('reviewer_id');
 
         if (
-            $reviewerIds->contains($transaction->buyer()->id) &&
-            $reviewerIds->contains($transaction->seller()->id)
+            $reviewerIds->contains($transaction->buyer->id) &&
+            $reviewerIds->contains($transaction->seller->id)
         ) {
             $transaction->update(['status' => 'completed']);
         }
